7ba88d619fa3cd50da34c38f35826006d6362cfa
ricky8554 <ricky.cheng850504@gmail.com>
2019-02-16 21:18:05 -0500

    hi

--- server.py
+++ server.py
@@ -1,25 +1,52 @@
 #!/usr/bin/python3
 # -*- coding: utf8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from camera import Camera
+
 app = Flask(__name__)
 
+
+user = {'username': 'Monkie'}
+emotion = {'emo': 'Happy'}
+posts = [
+    {
+        'author': {'username': 'Monkie'},
+        'body': 'Beautiful day in Durham!'
+    },
+    {
+        'author': {'username': 'Shawn'},
+        'body': 'The Avengers movie was so cool!'
+    }
+]
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/')
-@app.route('/index')
+@app.route('/start')
+def stanford_page():
+    return render_template('start.html', user=user)
+
+# @app.route('/')
+# def video_feed():
+#     return Response(gen(Camera()),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/index') 
+# what is this?
 def index():
-    user = {'username': 'Chao'}
-    emotion = {'emo': 'Happy'}
-    posts = [
-        {
-            'author': {'username': 'Monkie'},
-            'body': 'Beautiful day in Durham!'
-        },
-        {
-            'author': {'username': 'Shawn'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    
     return render_template('index.html', title='Home', user=user, posts=posts, emotion=emotion)
+
+
+
+
 
 
 @app.route('/my-link/')
